9d1cf8e73ca5984b01bd9b50230e16e30d7a0b51
Tyler Collins <tk11br@sharcnet.ca>
2025-11-17 12:20:38 -0500

    get mounted dir working and update reqs for mne

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import glob
+import os
+import mne
+import mne_bids
+
+MOUNT_PATH = "/data"
+
+class MontageRequest(BaseModel):
+    path: str
+    tmin: float
+    tmax: float
 
 app = FastAPI()
 
+
+# Remove this for security purposes later
 @app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+def read_mount():
+    return {"message": ', '.join(glob.glob(os.path.join(f'{MOUNT_PATH}/*')))}
