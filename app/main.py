ef80194e962c49524649f0a4ffa14fde8632cece
Tyler Collins <tk11br@sharcnet.ca>
2025-11-17 12:33:33 -0500

    add working concept for reading file

--- app/main.py
+++ app/main.py
@@ -1,18 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import glob
 import os
 import mne
 import mne_bids
+import json
 
 MOUNT_PATH = "/data"
 
-class MontageRequest(BaseModel):
+class TopoRequest(BaseModel):
     path: str
     tmin: float
     tmax: float
 
 app = FastAPI()
+
+@app.get("/montage")
+def read_montages(path: str) -> str:
+    try:
+        raw = mne.io.read_raw(path)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+    return ', '.join(raw.info['ch_names'])
 
 
 # Remove this for security purposes later
